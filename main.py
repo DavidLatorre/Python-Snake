a96cb1b56d08ba578d583c30ce722851d4338267
David Latorre <dvdn10071856@gmail.com>
2019-01-14 12:35:23 -0500

    Add base project

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Modules
-
+#Modules
 import pygame
-from pygame.locals import*
 
-# Constants
-
-# Classes
-# ---------------------------------------------------------------------
-
-# ---------------------------------------------------------------------
-
-# Funcitions
-
-# ---------------------------------------------------------------------
-
-# ---------------------------------------------------------------------
-
+#Main Function
 def main():
-    return 0
-
-if __name__ == '__main__':
     pygame.init()
+    logo = pygame.image.load("logo32x32.png")
+    pygame.display.set_icon(logo)
+    pygame.display.set_caption("minimal program")
+     
+    # create a surface on screen that has the size of 240 x 180
+    screen = pygame.display.set_mode((600,400))
+     
+    # define a variable to control the main loop
+    running = True
+     
+    # main loop
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+     
+     
+# run the main function only if this module is executed as the main script
+# (if you import this as a module then nothing is executed)
+if __name__=="__main__":
+    # call the main function
     main()
